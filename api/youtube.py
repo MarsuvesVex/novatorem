67e92cb4370cc91f00a64f48491588c76585ea28
Marsuves Vex <MarsuvesVex@TwitchTV>
2024-11-20 17:23:28 +1100

    --wip-- [skip ci]

--- api/youtube.py
+++ api/youtube.py
@@ -4,6 +4,7 @@ from flask import Blueprint, jsonify
 from api.config import Config
 import requests
 from dotenv import load_dotenv, find_dotenv
+import os
 
 youtube_bp = Blueprint('youtube', __name__)
 
